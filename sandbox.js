73b0f8257c657e0e8615c581c23de00a630da874
Lulama Cele <lulamac32@gmail.com>
2024-03-17 18:15:48 +0200

    added lesson 5 explicit types code

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,29 @@
-var character = 'luigi';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+//explicit
+var character;
+var age;
+var isLoggedIn;
+//age = luigi; this willn not work, age is declared to number type.
+age = 30;
+//  isLoggedIn = 25; this will not work, isLoggedIn is of type boolean not number
+isLoggedIn = false;
+//arrays
+var ninja = []; //initialize an array to an empty array to you can push to it oterwise it will not allow you to push to it.
+ninja.push('panda');
+//union types
+//union type lets you have variables, arrays, or objects with mixed types
+var mixed = []; //declaring a mixed array
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+var uid; //declaring normal variable to union type
+uid = 'ninja';
+uid = 12;
+// objects
+var ninjaOne;
+ninjaOne = { name: 'ninja', age: 32 };
+// two lines above are for a normal object
+var ninjaTwo; // we decalred an object with explicit types and mixed types
+ninjaTwo = { name: 'hero', age: 20, member: true };
+// ninjaTwo = {name: 'hero', age: 20, member: true, skills: []} this line will not work
+// we didn't declare an array when we were declaring ninjaTwo
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,43 @@
-const character = 'luigi';
-console.log(character);
+//explicit
+ let character: string;
+ let age: number;
+ let isLoggedIn: boolean;
 
-const inputs = document.querySelectorAll('input');
-console.log(inputs);
+ //age = luigi; this willn not work, age is declared to number type.
+age = 30;
+//  isLoggedIn = 25; this will not work, isLoggedIn is of type boolean not number
+isLoggedIn = false;
 
-inputs.forEach(input => {
-    console.log(input);
-});
+//arrays
+let ninja: string[] = []; //initialize an array to an empty array to you can push to it oterwise it will not allow you to push to it.
 
-//to compile use -> tsc sandbox.ts -w
-//this will compile and create a sandbox.js file that the browser can read
+ninja.push('panda');
+
+//union types
+//union type lets you have variables, arrays, or objects with mixed types
+let mixed : (string|number|boolean)[] = []; //declaring a mixed array
+mixed.push('hello');
+mixed.push(20);
+mixed.push(false);
+console.log(mixed);
+
+let uid : string|number; //declaring normal variable to union type
+uid = 'ninja';
+uid = 12;
+
+// objects
+let ninjaOne : object;
+ninjaOne = {name: 'ninja', age: 32};
+// two lines above are for a normal object
+
+let ninjaTwo: {
+    name: string,
+    age: number,
+    member: boolean
+} // we decalred an object with explicit types and mixed types
+
+ninjaTwo = {name: 'hero', age: 20, member: true}
+// ninjaTwo = {name: 'hero', age: 20, member: true, skills: []} this line will not work
+// we didn't declare an array when we were declaring ninjaTwo
+
+
